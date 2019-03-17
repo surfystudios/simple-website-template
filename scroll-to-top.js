4f54d2e5360f23f9621e049d34509658d8dc9130
josephine@spell.co <josephine@spell.co>
2019-03-17 18:04:07 +1100

    scroll completed with condition

---
+++ scroll-to-top.js
@@ -0,0 +1,26 @@
+// This button scrolls you back to the beginning of the scroll section
+$(document).ready(function(){
+  //notify when something scrolls
+  $(window).scroll(function(){
+   var y = $(document).scrollTop();
+    console.log(y);
+
+    if( y < 2700 || y > 4500){
+      console.log('button should show now');
+      // $('.button').fadeIn('slow');
+      $('.button').removeClass('show');
+    }
+    else {
+      console.log('scroll section has passed button should dissappear now');
+      // $('.button').fadeOut('slow');
+      $('.button').addClass('show');
+    }
+  });
+
+  $('.button').click(function(){
+    console.log('button clicked')
+     // $(document).scrollTop(0);
+    $('html, body').animate({scrollTop: 2700}, 2000);
+  })
+
+});
