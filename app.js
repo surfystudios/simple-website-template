139f8715223f5528ab189329c7c4e2a0fb92cf12
josephine@spell.co <josephine@spell.co>
2019-03-17 17:21:19 +1100

    adding lazyload

--- app.js
+++
@@ -1,8 +0,0 @@
-//Javascript for my entire project
-
-$(document).ready(function(){
-  console.log('document is ready!');
-  //TODO Write the code for the lazy loading images here..
-
-
-});
---
+++ lazyload.js
@@ -0,0 +1,35 @@
+//Javascript for my entire project
+
+var timeRemaining = 10;
+
+$(document).ready(function(){
+
+  var timer = setInterval(function(){
+       console.log("tick "+ timeRemaining);
+       timeRemaining--;
+
+    $("#count-down").html(timeRemaining);
+
+    if(timeRemaining == 0){
+      showImages();
+      clearInterval(timer);
+    }
+      
+  }, 1000);
+
+});
+
+function showImages(){
+
+  $("[data-img-path]").each(function(){
+
+      var theImage = $(this);
+      console.log(theImage);
+      // console.log(theImage.attr("id"));
+
+      var path = theImage.data('img-path');
+      theImage.attr("src", path);
+      theImage.removeClass("hidden");
+
+  });
+};
